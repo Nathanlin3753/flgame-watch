ea856e4b838bf65a8d90b59908b6690913879976
nathan <kaowei1996@gmail.com>
2026-09-08 19:23:49 +0800

    Auto-update market data: 2026-09-08 19:23:49

--- live-stamp.js
+++ live-stamp.js
@@ -1,2 +1,2 @@
-﻿window.__last_market_update = "2026-09-07 23:13:11";
-window.__last_checked_at = "2026-09-07 23:13:11";
+﻿window.__last_market_update = "2026-09-08 19:23:35";
+window.__last_checked_at = "2026-09-08 19:23:35";
